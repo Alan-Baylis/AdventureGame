13d468f059409767a6e249be55228f27816dd88f
zhoualex244 <az246@cornell.edu>
2017-03-27 01:46:52 -0400

    added AI Cat that spawns randomly throughout the map

--- Assets/Scripts/StartNormal.cs
+++ Assets/Scripts/StartNormal.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StartNormal : MonoBehaviour {
+public Transform cat;
 
 	// Use this for initialization
 	void Start () {
+		int x,z = 0;
 	MapGenerator mapGenerator = FindObjectOfType<MapGenerator>();
 		mapGenerator.DrawMapInEditor();
+		for (int i = 0; i < 200; i++) {
+			x = UnityEngine.Random.Range(-1000,1000);
+			z = UnityEngine.Random.Range(-1000,1000);
+			Instantiate(cat, new Vector3(x,80,z), Quaternion.identity);
+		}
+
+
 	}
 	
 	// Update is called once per frame
